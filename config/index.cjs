81f5a27ee1b433f8248100c6106649c9cc7db3b0
hlin0707 <139308352+hlin0707@users.noreply.github.com>
2023-07-11 22:36:24 -0700

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,9 +23,38 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ooskV6bHjk_0dcR4LD8jytHeDsU4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'vx8XMLVi0-GQEFVvGoYG23NkkjGoBsAEaqxbTjWZY_s',
+      useTemplateId: 'fWMP5UfoQff7AapxXChcbPZTTWMSuPRBuWZIWEpV5bw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-04',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '生日', name: '猪猪', year: '1993', date: '07-21',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '绵绵', year: '1998', date: '05-04',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'meet_day', date: '2023-04-19' },
+        // 牵手
+        { keyword: 'hand_day', date: '2023-05-28' },
+        // 相恋
+        { keyword: 'love_day', date: '2023-06-11' },
+      ],
+    },
+    {
+      // 想要发送的人的名字
+      name: '猪猪',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ooskV6bHjk_0dcR4LD8jytHeDsU4',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'fWMP5UfoQff7AapxXChcbPZTTWMSuPRBuWZIWEpV5bw',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '07-21',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
