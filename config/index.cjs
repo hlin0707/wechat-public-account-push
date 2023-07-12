82ce016719212298db57f00c7ae50e1ea479974f
hlin0707 <139308352+hlin0707@users.noreply.github.com>
2023-07-12 01:05:01 -0700

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ooskV6VHEJCGVj-01DVysWeJm6CU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '4_s3SEhhl00z_bNkMKAhL8NXwjaXStYZH1n3W_JPgDw',
+      useTemplateId: '-_Dyys2fXv5s3oaXP9gaanXUfg-TCZYY4m1uxItkJH0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-04',
       festivals: [
@@ -52,7 +52,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ooskV6bHjk_0dcR4LD8jytHeDsU4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '4_s3SEhhl00z_bNkMKAhL8NXwjaXStYZH1n3W_JPgDw',
+      useTemplateId: '-_Dyys2fXv5s3oaXP9gaanXUfg-TCZYY4m1uxItkJH0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-21',
       festivals: [
