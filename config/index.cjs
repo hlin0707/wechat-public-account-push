eb270b9235686ae74903d608d5c403e57bd12873
hlin0707 <139308352+hlin0707@users.noreply.github.com>
2023-07-11 21:53:32 -0700

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ooskV6bHjk_0dcR4LD8jytHeDsU4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'hGxPMxL18pjXbGt30OQ1UY5UqFO-XPDKkP3mHxabs_4',
+      useTemplateId: 'TB-Nk77t746Q53etXxu6_qywAWcsm1j7V7dZtwxz-iw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '5-4',
       festivals: [
@@ -55,7 +55,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'hGxPMxL18pjXbGt30OQ1UY5UqFO-XPDKkP3mHxabs_4',
+  CALLBACK_TEMPLATE_ID: 'TB-Nk77t746Q53etXxu6_qywAWcsm1j7V7dZtwxz-iw',
 
   CALLBACK_USERS: [
     {
